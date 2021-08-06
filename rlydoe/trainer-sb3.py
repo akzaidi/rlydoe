183a81d6a848946fe2e773ea995f5d0ea649de73
Ali Zaidi <alikaz.zaidi@gmail.com>
2021-08-06 22:45:31 +0000

    ADD: multiproc

--- rlydoe/trainer-sb3.py
+++ rlydoe/trainer-sb3.py
@@ -2,6 +2,7 @@
 # Set up fake display; otherwise rendering will fail
 
 import os
+from typing import Callable
 
 if not "DISPLAY" in os.environ:
     os.system("Xvfb :1 -screen 0 1024x768x24 &")
@@ -9,16 +10,23 @@ if not "DISPLAY" in os.environ:
 
 import datetime
 import gym
+import highway_env
 import logging
 from functools import partial
 from stable_baselines3 import PPO
 from stable_baselines3 import HerReplayBuffer, SAC, DDPG, TD3
 from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder
+from stable_baselines3.common.utils import set_random_seed
+from stable_baselines3.common.vec_env import (
+    DummyVecEnv,
+    VecVideoRecorder,
+    SubprocVecEnv,
+)
 from stable_baselines3.common.callbacks import (
     EvalCallback,
     StopTrainingOnRewardThreshold,
 )
+from stable_baselines3.common.env_util import make_vec_env
 from wandb.integration.sb3 import WandbCallback
 import wandb
 
@@ -36,15 +44,54 @@ def run_trainer(cfg: DictConfig):
 
     # define the environment
     # and monitor stats
-    def make_env(env_name):
+    # def make_env(env_name: str, num_instances: int = 1):
 
-        env = gym.make(env_name)
-        env = Monitor(env)
-        return env
+    #     env = gym.make(env_name)
+    #     env = Monitor(env)
+
+    #     return env
+    def make_env(
+        env_name: str, rank: int = 0, seed: int = 0, multi_proc: bool = False
+    ) -> Callable:
+        """
+        Utility function for multiprocessed env.
+        
+        :param env_name: (str) the environment ID
+        :param num_env: (int) the number of environment you wish to have in subprocesses
+        :param seed: (int) the inital seed for RNG
+        :param rank: (int) index of the subprocess
+        :return: (Callable)
+        """
+
+        if multi_proc:
+
+            def _init() -> gym.Env:
+                env = gym.make(env_name)
+                env = Monitor(env)
+                env.seed(seed + rank)
+                return env
+
+            set_random_seed(seed)
+            return _init
+        else:
+            env = gym.make(env_name)
+            env = Monitor(env)
+            return env
 
     # vectorize environment and record videos periodically
-    pmake_env = partial(make_env, env_name=cfg["environment"]["name"])
-    env = DummyVecEnv([pmake_env])
+    num_instances = int(cfg["environment"]["instances"])
+    if num_instances > 1:
+        logger.info(f"Asked to run {num_instances} using async")
+        # env = SubprocVecEnv(
+        #     [
+        #         make_env(env_name=cfg["environment"]["name"], rank=i, multi_proc=True)
+        #         for i in range(num_instances)
+        #     ]
+        # )
+        env = make_vec_env(cfg["environment"]["name"], n_envs=num_instances)
+    else:
+        pmake_env = partial(make_env, env_name=cfg["environment"]["name"])
+        env = DummyVecEnv([pmake_env])
     env = VecVideoRecorder(
         env, "videos", record_video_trigger=lambda x: x % 2000 == 0, video_length=200
     )
@@ -91,13 +138,24 @@ def run_trainer(cfg: DictConfig):
             logger.info(
                 f"Early stopping when reward reaches {cfg['environment']['max_reward']}"
             )
-            eval_env = gym.make(cfg["environment"]["name"])
+            # eval_env = gym.make(cfg["environment"]["name"])
+            peval_env = partial(make_env, env_name=cfg["environment"]["name"])
+            eval_env = DummyVecEnv([peval_env])
+            eval_env = VecVideoRecorder(
+                eval_env,
+                "videos",
+                record_video_trigger=lambda x: x % 2000 == 0,
+                video_length=200,
+                name_prefix="eval",
+            )
+            n_eval_episodes = 100
             callback_on_best = StopTrainingOnRewardThreshold(
-                reward_threshold=cfg["environment"]["max_reward"], verbose=1
+                reward_threshold=cfg["environment"]["max_reward"] * n_eval_episodes,
+                verbose=1,
             )
             eval_callback = EvalCallback(
                 eval_env,
-                n_eval_episodes=100,
+                n_eval_episodes=n_eval_episodes,
                 callback_on_new_best=callback_on_best,
                 verbose=1,
             )
@@ -114,13 +172,16 @@ def run_trainer(cfg: DictConfig):
     elif cfg["learner"]["name"].lower() == "sac":
         if cfg["learner"]["replay_buffer_class"].lower() == "her":
             replay_buffer_class = HerReplayBuffer
+            replay_buffer_kwargs = cfg["learner"]["replay_buffer_kwargs"]
         else:
             replay_buffer_class = None
+            replay_buffer_kwargs = None
 
         model = SAC(
             cfg["learner"]["policy_type"],
             env,
             replay_buffer_class=replay_buffer_class,
+            replay_buffer_kwargs=replay_buffer_kwargs,
             verbose=1,
             tensorboard_log=f"runs/{experiment_name}",
         )
@@ -128,7 +189,9 @@ def run_trainer(cfg: DictConfig):
         raise ValueError(f"Unknown learner: {cfg['learner']['name']}")
 
     model.learn(
-        total_timesteps=cfg["learner"]["total_timesteps"], callback=callback_list,
+        total_timesteps=cfg["learner"]["total_timesteps"]
+        * cfg["environment"]["instances"],
+        callback=callback_list,
     )
     model.save(f"models/{experiment_name}")
 
